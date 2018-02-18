67a7a4f5ba288cc7b6f7902899e18b08b87826d4
orzel1244 <orzel1244@gmail.com>
2018-02-18 16:45:22 +0100

    Fixed silentflash by putting it into loop

--- MultiHack/class/Misc/misc.cpp
+++ MultiHack/class/Misc/misc.cpp
@@ -10,6 +10,9 @@ void Misc::loop(){
     if(noFlash){
         m_memory->write(m_memory->read(m_memory->getModule("client.dll")+m_offsets->getAddress("dwLocalPlayer"))+m_offsets->getAddress("m_flFlashDuration"), 0);
     }
+    if(silentNoFlash){
+        m_memory->write<float>(m_memory->read(m_memory->getModule("client.dll")+m_offsets->getAddress("dwLocalPlayer"))+m_offsets->getAddress("m_flFlashMaxAlpha"), 60.0f);
+    }
     if(radar){
         DWORD player = m_memory->read(m_memory->getModule("client.dll")+
                                       m_offsets->getAddress("dwLocalPlayer"));
@@ -38,9 +41,7 @@ void Misc::setRadar(bool value){
 
 void Misc::setSilentNoFlash(bool value){
     silentNoFlash = value;
-    if(silentNoFlash){
-        m_memory->write<float>(m_memory->read(m_memory->getModule("client.dll")+m_offsets->getAddress("dwLocalPlayer"))+m_offsets->getAddress("m_flFlashMaxAlpha"), 60.0f);
-    } else {
+    if(!silentNoFlash){
         m_memory->write<float>(m_memory->read(m_memory->getModule("client.dll")+m_offsets->getAddress("dwLocalPlayer"))+m_offsets->getAddress("m_flFlashMaxAlpha"), 255.0f);
     }
 }
